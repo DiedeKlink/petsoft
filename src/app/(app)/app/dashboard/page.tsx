0c86f27346a40815560bf06496c785e46cf6da81
DiedeKlink <diede@protonmail.com>
2024-09-20 09:19:09 +0200

    Branding + Stats components

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -1,5 +1,14 @@
-import React from "react";
+import Branding from "@/components/branding";
+import H1 from "@/components/h1";
+import Stats from "@/components/stats";
 
 export default function Page() {
-  return <div>Dashboard</div>;
+  return (
+    <main>
+      <div className="flex items-center justify-between text-white py-8">
+        <Branding />
+        <Stats />
+      </div>
+    </main>
+  );
 }
---
+++ src/components/branding.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import H1 from "./h1";
+
+export default function Branding() {
+  return (
+    <section>
+      <H1>
+        Pet<span className="font-semibold">Soft</span>
+      </H1>
+      <p className="text-lg">Manage your pet daycare with easy</p>
+    </section>
+  );
+}
