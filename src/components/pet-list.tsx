9fd5a21d39a760531da6eceb3e211e9b803e14d7
DiedeKlink <diede@protonmail.com>
2024-09-21 09:44:48 +0200

    Petlist data fetching and typing

--- src/components/pet-list.tsx
+++ src/components/pet-list.tsx
@@ -1,33 +1,27 @@
+import { Pet } from "@/lib/types";
 import Image from "next/image";
 import React from "react";
 
-export default function PetList() {
+type PetListProps = {
+  pets: Pet[];
+};
+export default function PetList({ pets }: PetListProps) {
   return (
     <ul className="bg-white border-b border-black/[0.08]">
-      <li>
-        <button className="flex h-[70px] w-full cursor-pointer items-center px-5 text-base gap-3 hover:bg-[#eff1f2] focus:bg-[#eff1f2] transition">
-          <Image
-            src="https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png"
-            alt="Pet image"
-            width={45}
-            height={45}
-            className="rounded-full object-cover"
-          />
-          <p className="font-semibold">Benjamin</p>
-        </button>
-      </li>
-      <li>
-        <button className="flex h-[70px] w-full cursor-pointer items-center px-5 text-base gap-3 hover:bg-[#eff1f2] focus:bg-[#eff1f2] transition">
-          <Image
-            src="https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png"
-            alt="Pet image"
-            width={45}
-            height={45}
-            className="rounded-full object-cover"
-          />
-          <p className="font-semibold">Benjamin</p>
-        </button>
-      </li>
+      {pets.map((pet) => (
+        <li key={pet.id}>
+          <button className="flex h-[70px] w-full cursor-pointer items-center px-5 text-base gap-3 hover:bg-[#eff1f2] focus:bg-[#eff1f2] transition">
+            <Image
+              src={pet.imageUrl}
+              alt="Pet image"
+              width={45}
+              height={45}
+              className="rounded-full object-cover h-[45px]"
+            />
+            <p className="font-semibold">{pet.name}</p>
+          </button>
+        </li>
+      ))}
     </ul>
   );
 }
