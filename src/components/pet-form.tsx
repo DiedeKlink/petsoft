db0f9e8ba1620e0fa79a6599fd7c7933bb6e5eee
DiedeKlink <diede@protonmail.com>
2024-09-24 10:38:06 +0200

    Zod on Server

--- src/components/pet-form.tsx
+++ src/components/pet-form.tsx
@@ -10,44 +10,12 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { DEFAULT_PET_IMAGE } from "@/lib/constants";
+import { TPetForm, petFormSchema } from "@/lib/validations";
 
 type PetFormProps = {
   actionType: "add" | "edit";
   onFormSubmission: () => void;
 };
-
-const petFormSchema = z
-  .object({
-    name: z
-      .string()
-      .trim()
-      .min(3, "Name must be at least 3 characters")
-      .max(50),
-    ownerName: z
-      .string()
-      .trim()
-      .min(3, "Owner name must be at least 3 characters")
-      .max(50),
-    imageUrl: z.union([
-      z.literal(""),
-      z.string().url({ message: "Invalid URL" }),
-    ]),
-    age: z.coerce
-      .number()
-      .int()
-      .positive("Age must be a positive number")
-      .max(99, "Age must be less than 99"),
-    notes: z.union([
-      z.literal(""),
-      z.string().trim().max(1000, "Notes must be less than 1000 characters"),
-    ]),
-  })
-  .transform((data) => ({
-    ...data,
-    imageUrl: data.imageUrl || DEFAULT_PET_IMAGE,
-  }));
-
-type TPetForm = z.infer<typeof petFormSchema>;
 
 export default function PetForm({
   actionType,
---
+++ src/lib/validations.ts
@@ -0,0 +1,35 @@
+import { z } from "zod";
+import { DEFAULT_PET_IMAGE } from "./constants";
+
+export const petFormSchema = z
+  .object({
+    name: z
+      .string()
+      .trim()
+      .min(3, "Name must be at least 3 characters")
+      .max(50),
+    ownerName: z
+      .string()
+      .trim()
+      .min(3, "Owner name must be at least 3 characters")
+      .max(50),
+    imageUrl: z.union([
+      z.literal(""),
+      z.string().url({ message: "Invalid URL" }),
+    ]),
+    age: z.coerce
+      .number()
+      .int()
+      .positive("Age must be a positive number")
+      .max(99, "Age must be less than 99"),
+    notes: z.union([
+      z.literal(""),
+      z.string().trim().max(1000, "Notes must be less than 1000 characters"),
+    ]),
+  })
+  .transform((data) => ({
+    ...data,
+    imageUrl: data.imageUrl || DEFAULT_PET_IMAGE,
+  }));
+
+export type TPetForm = z.infer<typeof petFormSchema>;
