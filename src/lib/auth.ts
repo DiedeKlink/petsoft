7c6287da8e152d4f89837f21922bd1fdead872cb
DiedeKlink <diede@protonmail.com>
2024-09-24 20:21:53 +0200

    Credentials Provider (email/password) in Next Auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,11 +1,43 @@
+import prisma from "./db";
 import NextAuth, { NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
 const config = {
   pages: {
     signIn: "/login",
   },
-  providers: [],
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        // runs on login
+        const { email, password } = credentials;
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email,
+          },
+        });
+        if (!user) {
+          console.log("User not found");
+          return null;
+        }
+
+        const passwordsMath = await bcrypt.compare(
+          password,
+          user.hashedPassword
+        );
+        if (!passwordsMath) {
+          console.log("Invalid credentials");
+          return null;
+        }
+
+        return user;
+      },
+    }),
+  ],
   callbacks: {
     authorized: ({ request }) => {
+      //runs on every quest with middleware
       const isTryingToAccesApp = request.nextUrl.pathname.includes("/app");
 
       if (isTryingToAccesApp) {
