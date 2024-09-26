6eba503bb819dd05c0a54620ec58a69a85c47454
DiedeKlink <diede@protonmail.com>
2024-09-26 09:31:41 +0200

    Prisma Specific Errors

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -5,6 +5,7 @@ import prisma from "@/lib/db";
 import { checkAuth, getPetByPetId } from "@/lib/server-utils";
 import { sleep } from "@/lib/utils";
 import { authSchema, petFormSchema, petIdSchema } from "@/lib/validations";
+import { Prisma } from "@prisma/client";
 import bcrypt from "bcryptjs";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
@@ -48,12 +49,26 @@ export async function signUp(formData: unknown) {
 
   const { email, password } = validatedFormData.data;
   const hashedPassword = await bcrypt.hash(password, 10);
-  await prisma.user.create({
-    data: {
-      email,
-      hashedPassword,
-    },
-  });
+
+  try {
+    await prisma.user.create({
+      data: {
+        email,
+        hashedPassword,
+      },
+    });
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === "P2002") {
+        return {
+          message: "Email already in use",
+        };
+      }
+    }
+    return {
+      message: "Failed to sign up",
+    };
+  }
 
   await signIn("credentials", formData);
 }
