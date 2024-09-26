7167cf757b14ba04c2c1317d03faed1161ceac6e
DiedeKlink <diede@protonmail.com>
2024-09-26 12:10:16 +0200

    SearchParams after payment

--- src/app/(auth)/payment/page.tsx
+++ src/app/(auth)/payment/page.tsx
@@ -4,18 +4,29 @@ import { createCheckoutSession } from "@/actions/actions";
 import H1 from "@/components/h1";
 import { Button } from "@/components/ui/button";
 
-export default function Page() {
+export default function Page({ searchParams }) {
+  console.log(searchParams);
   return (
     <main className="flex flex-col items-center space-y-10">
       <H1>PetSoft acces requires payment</H1>
 
-      <Button
-        onClick={async () => {
-          await createCheckoutSession();
-        }}
-      >
-        Buy lifetime acces for $299
-      </Button>
+      {!searchParams.success && (
+        <Button
+          onClick={async () => {
+            await createCheckoutSession();
+          }}
+        >
+          Buy lifetime acces for $299
+        </Button>
+      )}
+
+      {searchParams.success && (
+        <div>
+          <p className="text-sm text-green-700">
+            Payment succesful! You now have lifetime acces to PetSoft.
+          </p>
+        </div>
+      )}
     </main>
   );
 }
